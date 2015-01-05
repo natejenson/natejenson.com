c1d3f59d67996f2de5bf58811e45824d7550609b
= <natejenson16@gmail.com>
2015-01-04 23:33:29 -0600

    - Remove flicker before navbar smooth scroll. - Bold navbar-brand

--- src/main/webapp/js/scroll.js
+++ src/main/webapp/js/scroll.js
@@ -3,8 +3,10 @@
  */
 
 function scrollNav(){
-	$(".nav a, .navbar-header a").click(function () {
-		// Scroll slowly please
+	$(".nav a").click(function (e) {
+		// Prevent the default action to avoid a the flicker before the animation.
+		e.preventDefault();
+		// Scroll slowly please.
 	    $('html, body').stop().animate({
 	        scrollTop: $( $(this).attr('href')).offset().top}, 500);
 	});
