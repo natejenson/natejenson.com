605d4965582144790314ed99c6327250b2b74498
Nate Jenson <nate.m.jenson@gmail.com>
2016-04-25 18:50:42 -0500

    converting site to nodeJS

--- Readme.md
+++
@@ -1,5 +0,0 @@
-# Hi, I'm Nate.
-
-This is the repo for my personal website. It's simple and to the point; Built with JQuery and Bootstrap.
-
-Want to connect? Here's my [LinkedIn](https://www.linkedin.com/in/natejenson).
---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.use('/', require('./routes/home'))
+app.use('/budget', require('./routes/budget'))
+app.use('/travel', require('./routes/travel'))
+
+app.listen(3000, function () {
+  console.log('App listening on port 3000...');
+});
