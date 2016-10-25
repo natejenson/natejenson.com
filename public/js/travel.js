1edef0cab5ef793ae19db4d891c566fa602843dd
Nate Jenson <nate.m.jenson@gmail.com>
2016-10-24 22:29:25 -0500

    Update travel map to latest Google Maps API

--- public/js/travel.js
+++ public/js/travel.js
@@ -1,4 +1,4 @@
-function initialize() {
+function initMap() {
 	var centerLatLng = new google.maps.LatLng(40.147439, -97.357952);
 	var mapOptions = {
 			zoom: 3,
@@ -13,91 +13,33 @@ function initialize() {
 	  if (map.getZoom() < minZoomLevel) map.setZoom(minZoomLevel);
 	});
 
-	var markerMN = new google.maps.Marker({
-		position: new google.maps.LatLng(44.934674, -93.220655),
-		map: map,
-		title: 'Minneapolis, Minnesota'
-	});
-	var markerWI = new google.maps.Marker({
-		position: new google.maps.LatLng(43.0500, -87.9500),
-		map: map,
-		title: 'Milwaukee, Wisconsin'
-	});
-	var markerGER = new google.maps.Marker({
-		position: new google.maps.LatLng(50.108883, 8.672956),
-		map: map,
-		title: 'Frankfurt, Germany'
-	});
-	var markerAUS = new google.maps.Marker({
-		position: new google.maps.LatLng(47.266820, 11.397673),
-		map: map,
-		title: 'Innsbruck, Austria'
-	});
-	var markerCAN = new google.maps.Marker({
-		position: new google.maps.LatLng(50.660225, -94.434688),
-		map: map,
-		title: 'Sydney Lake, Canada' 
-	});
-	var markerJAM = new google.maps.Marker({
-		position: new google.maps.LatLng(18.467229, -77.896886),
-		map: map,
-		title: 'Montego Bay, Jamaica' 
-	});
-	var markerAZ = new google.maps.Marker({
-		position: new google.maps.LatLng(33.398366, -111.820843),
-		map: map,
-		title: 'Mesa, Arizona' 
-	});
-	var markerWA = new google.maps.Marker({
-		position: new google.maps.LatLng(47.605127, -122.334533),
-		map: map,
-		title: 'Seattle, Washington' 
-	});
-	var markerFL = new google.maps.Marker({
-		position: new google.maps.LatLng(26.623194, -81.850851),
-		map: map,
-		title: 'Ft. Myers, Florida' 
-	});
-	var markerIL = new google.maps.Marker({
-		position: new google.maps.LatLng(41.876885, -87.619804),
-		map: map,
-		title: 'Chicago, Illinois' 
-	});
-	var markerMT = new google.maps.Marker({
-		position: new google.maps.LatLng(45.374878, -107.430519),
-		map: map,
-		title: 'Yellowstone National Park, Montana'
-	});
-	var markerApostle = new google.maps.Marker({
-		position: new google.maps.LatLng(46.939149, -90.632593),
-		map: map,
-		title: 'Apostle Islands, Lake Superior' 
-	});
-	var markerSD = new google.maps.Marker({
-		position: new google.maps.LatLng(43.879141, -103.459100),
-		map: map,
-		title: 'Mount Rushmore, South Dakota' 
-	});
-	var markerND = new google.maps.Marker({
-		position: new google.maps.LatLng(46.854198, -96.819587),
-		map: map,
-		title: 'Fargo, North Dakota' 
-	});
-	var markerBWCA = new google.maps.Marker({
-		position: new google.maps.LatLng(47.881536, -90.888649),
-		map: map,
-		title: 'BWCA, Minnesota' 
-	});
-	var markerGERMun = new google.maps.Marker({
-		position: new google.maps.LatLng(48.057541, 11.631868),
-		map: map,
-		title: 'Munich, Germany' 
-	});
-	var markerWinnipeg = new google.maps.Marker({
-		position: new google.maps.LatLng(49.894432, -97.138283),
-		map: map,
-		title: 'Winnipeg, Manitoba' 
-	});
-}
+	var locations = [
+		['Minneapolis, Minnesota', 44.980717, -93.252126],
+		['Milwaukee, Wisconsin', 43.0500, -87.9500],
+		['Frankfurt, Germany', 50.108883, 8.672956],
+		['Innsbruck, Austria', 47.266820, 11.397673],
+		['Sydney Lake, Canada', 50.660225, -94.434688],
+		['Montego Bay, Jamaica', 18.467229, -77.896886],
+		['Mesa, Arizona', 33.398366, -111.820843],
+		['Seattle, Washington', 47.605127, -122.334533],
+		['Ft. Myers, Florida', 26.623194, -81.850851],
+		['Chicago, Illinois', 41.876885, -87.619804],
+		['Yellowstone National Park, Montana', 45.374878, -107.430519],
+		['Apostle Islands, Lake Superior', 46.939149, -90.632593],
+		['Mount Rushmore, South Dakota', 43.879141, -103.459100],
+		['Fargo, North Dakota', 46.854198, -96.819587],
+		['Boundary Waters Canoe Area, Minnesota', 47.881536, -90.888649],
+		['Munich, Germany', 48.057541, 11.631868],
+		['Denver, Colorado', 39.733322, -104.992370],
+		['Rocky Mountain National Park', 40.329900, -105.704635]
+	];
 
-google.maps.event.addDomListener(window, 'load', initialize);
+	
+	for (var i = 0; i < locations.length; i++){
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+			title: locations[i][0]
+		});
+		marker.setMap(map);
+	}
+}
